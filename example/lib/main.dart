53226521b26a9eac514af25ad6d1907e2a17079f
isaac553876299 <60979696+isaac553876299@users.noreply.github.com>
2021-12-17 09:21:01 +0100

    firebase added

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,7 +1,11 @@
 import 'package:example/screens/home_page.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
