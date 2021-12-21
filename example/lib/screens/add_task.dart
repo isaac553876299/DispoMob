cdd1686c262d596ef184249deb25c03628b269de
isaacdd <isaacdd.bcn.2001@gmail.com>
2021-12-21 14:19:54 +0100

    sda

--- example/lib/screens/add_task.dart
+++ example/lib/screens/add_task.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:example/models/task_model.dart';
 
 class AddTaskScreen extends StatefulWidget {
   const AddTaskScreen({Key? key}) : super(key: key);
@@ -8,17 +9,19 @@ class AddTaskScreen extends StatefulWidget {
 }
 
 class _AddTaskScreenState extends State<AddTaskScreen> {
-  late TextEditingController controller;
+  late TextEditingController controller1, controller2;
 
   @override
   void initState() {
     super.initState();
-    controller = TextEditingController();
+    controller1 = TextEditingController();
+    controller2 = TextEditingController();
   }
 
   @override
   void dispose() {
-    controller.dispose();
+    controller1.dispose();
+    controller2.dispose();
     super.dispose();
   }
 
@@ -28,21 +31,62 @@ class _AddTaskScreenState extends State<AddTaskScreen> {
       appBar: AppBar(
         title: Text("Add Task"),
       ),
-      body: Column(
-        children: [
-          Row(
-            children: [
-              Text('title: '),
-              TextField(controller: controller),
-            ],
-          ),
-          Row(
-            children: [
-              Text('priority: '),
-              TextField(controller: controller),
-            ],
-          ),
-        ],
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20),
+        child: Column(
+          children: [
+            Row(
+              children: [
+                Text(
+                  'title: ',
+                  style: TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.w500,
+                    color: Colors.grey[700],
+                  ),
+                ),
+                SizedBox(width: 10),
+                Expanded(child: TextField(controller: controller1)),
+              ],
+            ),
+            Row(
+              children: [
+                Text(
+                  'priority: ',
+                  style: TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.w500,
+                    color: Colors.grey[700],
+                  ),
+                ),
+                SizedBox(width: 10),
+                Expanded(child: TextField(controller: controller2)),
+              ],
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              child: Text(
+                'Save',
+                style: TextStyle(
+                  fontSize: 32,
+                ),
+              ),
+              onPressed: () {
+                setState(
+                  () {
+                    if (controller1.text.isNotEmpty &&
+                        controller2.text.isNotEmpty) {
+                      Task t = Task(controller1.text, DateTime.now(), [],
+                          int.parse(controller2.text)); //some input gives error
+                      //save to firebase here
+                      Navigator.of(context).pop();
+                    }
+                  },
+                );
+              },
+            ),
+          ],
+        ),
       ),
     );
   }
